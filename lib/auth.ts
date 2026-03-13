29621b19178c35916b325eba524f1b50ab458daf
Gianluca Esposito <g@esposi.to>
2026-03-13 21:34:02 +0100

    Init from create-auth-skill

---
+++ lib/auth.ts
@@ -0,0 +1,20 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+import { organization, admin, bearer } from "better-auth/plugins";
+import { db } from "./db";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+  },
+  plugins: [organization(), admin(), bearer(), nextCookies()],
+});
+
+export type Session = typeof auth.$Infer.Session;
